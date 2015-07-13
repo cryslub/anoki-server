ac0c1f0478c64a7b00e2f93fc09c381fd684b767
SJ Lee <cryslub@hotmail.com>
2015-07-13 11:16:10 +0900

    sms api 추가

--- anoki/src/com/anoki/resources/AuthResource.java
+++ anoki/src/com/anoki/resources/AuthResource.java
@@ -13,6 +13,7 @@ import com.anoki.jaxb.Response;
 import com.anoki.jaxb.User;
 import com.anoki.singleton.AuthCodes;
 import com.anoki.singleton.Ibatis;
+import com.gabia.api.ApiClass;
 
 
 @Path("auth")
@@ -30,6 +31,23 @@ public class AuthResource {
 		AuthCodes.map.put(phone.country+phone.number+phone.device,number);
 		
 		//sms 전송
+
+		String api_id = "goodwill2015";		// sms.gabia.com 이용 ID
+		String api_key = "_API_KEY_";	// 환결설정에서 확인 가능한 SMS API KEY
+		
+		ApiClass api = new ApiClass(api_id, api_key);
+		
+		// 단문 발송 테스트
+		String arr[] = new String[7];
+		arr[0] = "sms";								// 발송 타입 sms or lms
+		arr[1] = "_UNIQUE_KEY_";				// 결과 확인을 위한 KEY ( 중복되지 않도록 생성하여 전달해 주시기 바랍니다. )
+		arr[2] = "_TITLE_";							//  LMS 발송시 제목으로 사용 SMS 발송시는 수신자에게 내용이 보이지 않음.
+		arr[3] = "아노키 인증번호 "+number;					// 본문 (90byte 제한)
+		arr[4] = "";			// 발신 번호
+		arr[5] = phone.number;				// 수신 번호
+		arr[6] = "0";									//예약 일자 "2013-07-30 12:00:00" 또는 "0" 0또는 빈값(null)은 즉시 발송 
+		
+		String responseXml = api.send(arr);
 		
 		Response r = new Response();
 		r.result = "0";
