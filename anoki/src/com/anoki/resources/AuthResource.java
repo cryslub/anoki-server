b88b97ff76d72c408548e32a3e6b9e5658514a8e
SJ Lee <cyslub@hotmail.com>
2015-07-26 16:25:21 +0900

    2015.7.25

--- anoki/src/com/anoki/resources/AuthResource.java
+++ anoki/src/com/anoki/resources/AuthResource.java
@@ -1,6 +1,10 @@
 package com.anoki.resources;
 
+import java.math.BigInteger;
+import java.net.PasswordAuthentication;
+import java.security.SecureRandom;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -8,12 +12,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+
+import com.anoki.jaxb.Message;
 import com.anoki.jaxb.Phone;
 import com.anoki.jaxb.Response;
+import com.anoki.jaxb.Search;
 import com.anoki.jaxb.User;
 import com.anoki.singleton.AuthCodes;
 import com.anoki.singleton.Ibatis;
+import com.anoki.singleton.Keys;
 import com.gabia.api.ApiClass;
+
+
+
 
 
 @Path("auth")
@@ -90,6 +101,17 @@ public class AuthResource {
 		
 		try {
 			r.result = (String) Ibatis.object("getAccountWithPhone",phone);
+			if(r.result != null){
+				String[] arr = r.result.split("@");
+				int total = arr[0].length();
+				String screened  = arr[0].substring(0, total/2);
+				for(int i =0;i<total - total/2;i++){
+					screened += "*";
+				}
+				
+				r.result = screened + "@" + arr[1];
+				
+			}
 		
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -103,12 +125,16 @@ public class AuthResource {
 	@Path("pass")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)	
-	public Response pass(User user){
+	public Response pass(Search search){
+		
+		
+		search.id = Keys.getUserId(search.apiKey);
+
 		
 		Response r = new Response();
 		
 		try {
-			Integer id = (Integer) Ibatis.object("getUserId",user);
+			Integer id = (Integer) Ibatis.object("confirmPass",search);
 			
 			if(id!=null){
 				r.id = id;
@@ -142,5 +168,76 @@ public class AuthResource {
 		
 		return r;
 	}
+	
+	@POST
+	@Path("restore")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)	
+	public Response restore(Search search){
+		
+		search.id = Keys.getUserId(search.apiKey);
+		
+		Response r = new Response();
+		
+		try {
+			User user = new User();
+			user.id = search.id;
+			user = (User) Ibatis.object("getUser",user);
+			SecureRandom random = new SecureRandom();
+			String newPass = new BigInteger(130, random).toString(32);			
+			user.pass = newPass.substring(15);
+			Ibatis.update("updateUser",user);
+			
+			sendEmail(user.account,user.pass);
+			
+			
+			r.result="0";
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return r;
+	}
+	
+	private void sendEmail(String email, String pass){
+		
+		final String username = "cinderellamessenger@gmail.com";
+		final String password = "goodwill1004";
+ 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+ 
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("noreply@anoki.co.kr"));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse(email));
+			message.setSubject("아노키 비밃번호 재설정");
+			message.setText("변경된 비밀번호 입니다.,"
+				+ "\n\n "+pass);
+ 
+			Transport.send(message);
+ 
+			System.out.println("Done");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
 	
 }
